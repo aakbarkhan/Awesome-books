c1d3302c79d7074a2af523b6dff885f7725b5a52
aakbarkhan <aakuukhan@gmail.com>
2021-10-26 14:06:57 +0530

    Add js

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-console.log('Hello World!')
-
-let bookList = [];
+let bookList = [
+    {title:'God must be crazy', 
+    author:'Ali Baba'}
+];
 
 const books = document.getElementById('books-name');
 const submitBtn = document.getElementById('submit');
-console.log(books);
+const titleValue = document.getElementById('name');
+const authorValue = document.getElementById('author');
 
 function addBooks() {
-    books.innerHTML += `
-    <h1 class="book-title">Title</h1>
-    <h2 class="book-title">Author</h2>
-    <button type="submit">Remove</button>
-    `
+    bookList.push({title:titleValue.value, author:authorValue.value});    
 }
 
-function removeBooks() {
-    const books = document.getElementsByClassName('book-title');
-    const title = document.getElementsByClassName('book-title');
+//  removeBooks(id)
+function removeBooks(id) {
+       const filterbookList = bookList.filter((e) => id !== bookList.length - 1);
+    bookList = filterbookList;
+    console.log(id)
+    display();
+}
+function display() {
+   
+    addBooks();
+    document.querySelector('#books-name').innerHTML = '';
+    bookList.forEach((book) => {
+        books.innerHTML += `
+        <h1  class="book-title">Title: ${book.title}</h1>
+        <h2 class="book-title">Author: ${book.author}</h2>
+        <button id=${bookList.length-1}  onClick=removeBooks(id) type="submit">Remove</button>
+        `;
+    });
+    titleValue.value = "";
+    authorValue.value = "";
+
 }
 
-submitBtn.addEventListener('click', addBooks)
+submitBtn.addEventListener('click', display);
